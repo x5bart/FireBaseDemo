0f7c4d2f55bb75a4c86b1d30cf5094a002fa9f29
Pavel Kovalchuk <x5bart@yandex.ru>
2020-02-08 19:49:04 +0300

    Initial commit

---
+++ app/src/main/java/com/x5bart/firebasedemo/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.x5bart.firebasedemo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
+        btnSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser: FirebaseUser?) {
+
+    }
+}
---
+++ app/src/main/java/com/x5bart/firebasedemo/SignUpActivity.kt
@@ -0,0 +1,59 @@
+package com.x5bart.firebasedemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        auth = FirebaseAuth.getInstance()
+
+        btnSign.setOnClickListener {
+            signUpUser()
+
+        }
+    }
+
+   private fun signUpUser() {
+        if (etEMail.text.toString().isEmpty()) {
+            etEMail.error = "Please enter valid email"
+            etEMail.requestFocus()
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(etEMail.text.toString()).matches()) {
+            etEMail.error = "Please enter valid email"
+            etEMail.requestFocus()
+            return
+        }
+
+        if (etPassword.text.toString().isEmpty()) {
+            etPassword.error = "Please enter valid email"
+            etPassword.requestFocus()
+            return
+        }
+
+       auth.createUserWithEmailAndPassword(etEMail.text.toString(), etPassword.text.toString())
+           .addOnCompleteListener(this) { task ->
+               if (task.isSuccessful) {
+                   // Sign in success, update UI with the signed-in user's information
+                   startActivity(Intent(this,LoginActivity::class.java))
+                   finish()
+               } else {
+                   // If sign in fails, display a message to the user.
+                   Toast.makeText(baseContext, "Authentication failed. Try again after some time.",
+                       Toast.LENGTH_SHORT).show()
+               }
+
+               // ...
+           }
+    }
+}
